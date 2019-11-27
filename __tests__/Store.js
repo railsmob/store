22eb3c3d69c5571c43949fdf4975cf1abe5d3469
Ivan Zotov <ivan_zotov@railsmob.com>
2019-11-27 20:34:22 +0300

    Implement update method for store

--- __tests__/Store.js
+++ __tests__/Store.js
@@ -1,5 +1,24 @@
-import Store from '../src/Store';
+import Store from "../src/Store";
 
-describe('Store', () => {
-  it.todo('should pass');
+describe("Store", () => {
+  it("should has a default state as an empty object", () => {
+    const store = new Store();
+    expect(store.state).toEqual({});
+  });
+
+  describe(".update", () => {
+    it("should emit will_update and update events", () => {
+      const store = new Store();
+      store.state = { will: "update" };
+      const willUpdate = jest.fn();
+      const update = jest.fn();
+      store.on("will_update", willUpdate);
+      store.on("update", update);
+      const callback = jest.fn(state => ({ ...state, did: "update" }));
+      store.update(callback);
+      expect(callback).toHaveBeenCalledTimes(1);
+      expect(willUpdate).toHaveBeenCalledWith({ will: "update" });
+      expect(update).toHaveBeenCalledWith({ will: "update", did: "update" });
+    });
+  });
 });
--- src/Store.js
+++ src/Store.js
@@ -1,2 +1,17 @@
+import events from "@railsmob/events";
+
 export default class Store {
+  state = {};
+
+  // aliases
+  on = (eventId, ...other) => events.on(`store_${eventId}`, ...other);
+  once = (eventId, ...other) => events.once(`store_${eventId}`, ...other);
+  off = (eventId, ...other) => events.off(`store_${eventId}`, ...other);
+  emit = (eventId, ...other) => events.emit(`store_${eventId}`, ...other);
+
+  update = fn => {
+    this.emit("will_update", this.state);
+    this.state = fn(this.state);
+    this.emit("update", this.state);
+  };
 }
